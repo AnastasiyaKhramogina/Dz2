571b507da9f96b80b4107ae456d13b06d4678d0b
Anastasya Khramogina <akhramogina@mail.ru>
2023-03-09 19:55:17 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 20;
+        int s = x / y;
+        if (s > 1) {
+            System.out.println(s);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
